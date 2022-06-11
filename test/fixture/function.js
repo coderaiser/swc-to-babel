02db832466a434ba300fd0d9bf5d1a8b489336e4
coderaiser <coderaiser@cloudcmd.io>
2022-06-11 16:08:31 +0300

    fix: swc-to-babel: ArrowFunctionExpression

--- test/fixture/function.js
+++ test/fixture/function.js
@@ -1,2 +1,5 @@
 function __(__args){}
-const a = function (__args){}
+const a = function (__args){};
+const b = function x(__args){};
+(__args__a) => __b(__args__a);
+(__args__a) => {__b(__args__a);}
