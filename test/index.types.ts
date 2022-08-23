c7d48bf35a7b4ac4c40d450790207bdc8e0998d8
Markos-Th09 <Markos-Th09@users.noreply.github.com>
2022-08-23 07:53:12 +0000

    chore: swc-to-babel: lint using actions ☘️

--- test/index.types.ts
+++ test/index.types.ts
@@ -1,8 +1,1 @@
-import {ParseResult} from '@babel/parser';
-import {File} from '@babel/types';
-import {parseSync} from '@swc/core';
-import toBabel from '../';
 
-const code = 'const f = ({a}) => a;';
-
-const result: ParseResult<File> = toBabel(parseSync(code), code);
