6e05abbc2c1e30ed3ba557ae000220532a435818
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-08-23 19:15:36 +0300

    make source an optional parameter

--- lib/swc-to-babel.d.ts
+++ lib/swc-to-babel.d.ts
@@ -2,4 +2,4 @@ import {ParseResult} from '@babel/parser';
 import {File} from '@babel/types';
 import type {Module} from '@swc/core';
 
-export default function toBabel(ast: Module, src: string): ParseResult<File>;
+export default function toBabel(ast: Module, src?: string): ParseResult<File>;
