2769686cd12f9c8dcde1dbd8b97880eaa4f7d0d0
coderaiser <coderaiser@cloudcmd.io>
2025-03-30 19:33:41 +0300

    chore: lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -3,13 +3,13 @@ import {run} from 'madrun';
 export default {
     'wisdom': () => run(['lint', 'coverage']),
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
-    'watch:test': async () => `nodemon -w lib -w test -x ${await run('test')}`,
+    'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
     'lint': () => `putout .`,
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
     'coverage': async () => `c8 ${await run('test')}`,
     'report': () => 'c8 report --reporter=lcov',
-    'fixture': async () => `UPDATE=1 ${await run('test')}`,
+    'fixture': async () => `UPDATE=1 "${await run('test')}"`,
     'test:dts': () => 'check-dts test/*.errors.ts',
 };
---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import {matchToFlat} from '@putout/eslint-flat';
+import {defineConfig} from 'eslint/config';
+import {safeAlign} from 'eslint-plugin-putout';
+
+export const match = {
+    '*md{ts}': {
+        'n/no-unsupported-features/node-builtins': 'off',
+    },
+};
+
+export default defineConfig([
+    safeAlign, {
+        'rules': {
+            'no-useless-return': 'off',
+        },
+    },
+    ...matchToFlat(match),
+]);
